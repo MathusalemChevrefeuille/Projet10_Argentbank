59b18c312da9b3fbceca28a3b39e734c2cece97d
MathusalemChevrefeuille <161305006+MathusalemChevrefeuille@users.noreply.github.com>
2024-07-01 17:59:39 +0200

    final commît

---
+++ Frontend/src/components/login/loginfunc.js
@@ -0,0 +1,79 @@
+import {setToken,setfirstName,setlastName,setuserName,logout} from "../../Slice/loginslice"
+import store from "../store";
+
+export async function LoginUser (logs, dispatch, navigate){
+    try{
+        const response = await fetch("http://localhost:3001/api/v1/user/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(logs),
+        });
+
+        if (response.status === 400) {
+            alert("La combinaison de l'adresse mail et/ou du mot de passe est mauvaise.");
+            return;
+        }
+
+        const retour = await response.json()
+
+        dispatch(setToken({token: retour.body.token}));
+        const userProfile = await UserProfile(retour.body.token);
+        dispatch(setuserName(userProfile.userName));
+        dispatch(setfirstName(userProfile.firstName));
+        dispatch(setlastName(userProfile.lastName));
+        navigate("/accountpage");
+    }
+    catch (error){
+        console.error(error);
+        throw error;
+    }
+}
+
+
+async function UserProfile (token) {
+    try{
+        const response = await fetch ("http://localhost:3001/api/v1/user/profile", {
+            method: "POST",
+        headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+        }
+        });
+        const retour = await response.json();
+        return retour.body;
+    }
+    catch (error) {
+        console.error(error);
+        throw error;    
+    }
+}
+
+export async function ChangeUsername (newusername, dispatch,token){
+    try {
+        const response = await fetch("http://localhost:3001/api/v1/user/profile", {
+            method: "PUT",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(
+                {"username": newusername}
+            )
+            });
+            const retour = await response.json();
+            dispatch(setuserName({ userName: newusername }));
+            return retour.body;
+        }
+        catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+export function Logout(navigate, dispatch) {
+    return new Promise((resolve, reject) => {
+        dispatch(logout());
+        resolve(navigate("/")); 
+    });
+}
+
